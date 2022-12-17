513326f6f06d528bbccbe65fbffd94392880a5dc
Sergei_Solovev <panika944@gmail.com>
2022-12-17 11:53:02 +0100

    create hook useTypedSelector, create action creator user.ts, create component UserList and inside this component dispatch(fetchUser())

---
+++ src/components/UserList.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store/action-creators/user";
+
+const UserList: React.FC = () => {
+  const { users, error, loading } = useTypedSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch<any>(fetchUsers())
+  }, [])
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>{error}</h1>
+
+  return (
+    <div>
+      {users.map(user =>
+        <div key={user.id}>{user.name}</div>
+      )}
+    </div>
+  );
+};
+
+export default UserList;
