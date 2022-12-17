a2ae932839cea7fc9433b3c3343445d5d46c3d22
Sergei_Solovev <panika944@gmail.com>
2022-12-17 17:01:45 +0100

    create component TodoList, create hook useActions, create action-creators todo and combine (todo action-creator + user) => index.ts, create todoRedcuer, create todo type

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { useTypedSelector } from "../hooks/useTypedSelector";
-import { useDispatch } from "react-redux";
 import { fetchUsers } from "../store/action-creators/user";
+import { useActions } from "../hooks/useActions";
 
 const UserList: React.FC = () => {
   const { users, error, loading } = useTypedSelector(state => state.user);
-  const dispatch = useDispatch();
+  const { fetchUsers } = useActions()
 
   useEffect(() => {
-    dispatch<any>(fetchUsers())
+    fetchUsers()
   }, [])
 
   if (loading) return <h1>Loading...</h1>
